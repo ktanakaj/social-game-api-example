4d4990f30100dccc1b6015755c11c9ef4242696c
Koichi Tanaka <ktanakaj@users.noreply.github.com>
2019-05-10 10:42:50 +0900

    laravel-model-caching拡張について補足コメントを追記

--- server/app/Models/Masters/MasterCachedBuilder.php
+++ server/app/Models/Masters/MasterCachedBuilder.php
@@ -23,6 +23,9 @@ class MasterCachedBuilder extends CachedBuilder
         string $method
     ) {
         // ※ laravel-model-cachingのメソッドを改造して、有効期限を指定するようにしたバージョン
+        // （ライブラリ的には、有効期限ではなく、キャッシュ用のRedisにmax memory policyを変えて運用する想定らしいが、
+        //   セッション等と1台のRedisを共用する場合にポリシーを分けられないので、期限を付ける。
+        //   https://github.com/GeneaLabs/laravel-model-caching/issues/243 ）
         $this->checkCooldownAndRemoveIfExpired($this->model);
 
         return $this->cache($cacheTags)
