62b39a6db415c5c7da612fc46fcaa8478cbcfaf0
Koichi Tanaka <ktanakaj@users.noreply.github.com>
2019-12-09 15:02:18 +0900

    バイナリを受ける/返すAPIでアクセスログが壊れるのを修正

--- server/app/Http/Middleware/AccessLog.php
+++ server/app/Http/Middleware/AccessLog.php
@@ -10,6 +10,18 @@ use Illuminate\Http\JsonResponse;
 class AccessLog
 {
     /**
+     * ログに出力するコンテンツ種別（正規表現可。正規表現の場合 | から初めてください）。
+     * @var array
+     */
+    protected $contentTypes = [
+        'application/json',
+        '|^text/|',
+        'application/xml',
+        'application/javascript',
+        'application/x-javascript',
+    ];
+
+    /**
      * リクエストハンドラー。
      * @param \Illuminate\Http\Request $request リクエスト。
      * @param \Closure $next 次のリクエストハンドラー。
@@ -20,7 +32,7 @@ class AccessLog
         // アクセス開始時の時間を記録して処理実行、ログ出力
         $starttime = microtime(true);
         $response = $next($request);
-        self::log($request, $response, $starttime);
+        $this->log($request, $response, $starttime);
         return $response;
     }
 
@@ -30,7 +42,7 @@ class AccessLog
      * @param \Symfony\Component\HttpFoundation\Response $res HTTPレスポンス。
      * @param float $starttime 処理時間測定用の開始時のmicrotime。
      */
-    private static function log($req, $res, float $starttime = null) : void
+    private function log($req, $res, float $starttime = null) : void
     {
         try {
             // ステータスコードによってログレベルを切り替え
@@ -71,15 +83,27 @@ class AccessLog
             // リクエストボディ/レスポンスボディ
             // ※ StreamedResponseなどのcontentが取れないレスポンスは現状空扱い。contentLengthも0になる。
             $resBody = $res->getContent();
-            $context['res']['contentLength'] = strlen($resBody);
+            $context['res']['contentLength'] = $resSize = strlen($resBody);
             if (config('app.debug')) {
+                $reqType = $req->headers->get('CONTENT_TYPE', '');
                 $reqBody = $req->getContent();
-                if ($req->isJson()) {
-                    $reqBody = self::hidePasswordLog($reqBody);
+                if (strlen($reqBody) === 0) {
+                    $reqBody = '';
+                } elseif (!$this->isLoggableBody($reqType)) {
+                    $reqBody = "({$reqType})";
+                } elseif ($req->isJson()) {
+                    $reqBody = $this->hidePasswordLog($reqBody);
                 }
-                if ($res instanceof JsonResponse) {
-                    $resBody = self::hidePasswordLog($resBody);
+
+                $resType = $res->headers->get('CONTENT_TYPE', '');
+                if ($resSize === 0) {
+                    $resBody = '';
+                } elseif (!$this->isLoggableBody($resType)) {
+                    $resBody = "({$resType})";
+                } elseif ($res instanceof JsonResponse) {
+                    $resBody = $this->hidePasswordLog($resBody);
                 }
+
                 $context['req']['content'] = $reqBody;
                 $context['res']['content'] = $resBody;
             }
@@ -91,13 +115,33 @@ class AccessLog
     }
 
     /**
+     * ログ出力可能なコンテンツ種別か？
+     * @param string $type コンテンツ種別。
+     * @return bool 可能な場合true。
+     */
+    protected function isLoggableBody(string $type) : bool
+    {
+        foreach ($this->contentTypes as $condition) {
+            // 先頭文字が | の場合正規表現扱い
+            if (strpos($condition, '|') === 0) {
+                if (preg_match($condition, $type)) {
+                    return true;
+                }
+            } elseif ($type === $condition) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
      * JSONログ上のパスワードを隠す。
      * @param string $log JSONログ文字列。
      * @return string パスワードを置換したログ文字列。
      */
-    private static function hidePasswordLog(string $log) : string
+    private function hidePasswordLog(string $log) : string
     {
-        // TODO: パスワードに"が含まれていると中途半端になるかも
+        // TODO: パスワードに"が含まれていると中途半端になるかも。デバッグ以外で使う場合は要確認
         if ($log === null) {
             return $log;
         }
